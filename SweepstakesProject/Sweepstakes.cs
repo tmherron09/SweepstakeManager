01bf6639b1bae9e984fab7f5a9ce71bc3c05a062
TimH <tmherron09@gmail.com>
2020-08-03 13:39:19 -0500

    Add partial implementations for the Sweepstakes Class
    Missing UI logic and logic to randomly select a winner.

--- SweepstakesProject/Sweepstakes.cs
+++ SweepstakesProject/Sweepstakes.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace SweepstakesProject
 {
-    class Sweepstakes
+    public class Sweepstakes
     {
+        /// <summary>
+        /// Dictionary containing all Registered Contestants.
+        /// </summary>
+        /// <remarks></remarks>
+        Dictionary<int, Contestant> contestants;
+        private string name;
+        public string Name { get; set; }
+        /// <summary>
+        /// Constructor for Sweepstakes.
+        /// </summary>
+        /// <param name="name">Name of Sweepstakes</param>
+        public Sweepstakes(string name)
+        {
+            this.name = name;
+        }
+
+        public void RegisterContestant(Contestant contestant)
+        {
+            contestants.Add(contestant.RegistrationNumber, contestant);
+            // Throws exception if same contestant is entered twice.
+        }
+        public Contestant PickWinner()
+        {
+            //Add Logic to pick a winner.
+
+            contestants.TryGetValue(1, out Contestant winner);
+            // Placeholder returns first Contestant.
+            return winner;
+        }
+        public void PrintContestantInfo(Contestant contestant)
+        {
+            // Send call to a physical printer.
+            // Send call to UI.DisplayContestantInfo()
+            UI.DisplayContestantInfo(contestant);
+
+        }
+            
     }
 }
