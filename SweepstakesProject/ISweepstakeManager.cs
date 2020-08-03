90b47d138ec070d5f9bcf8f658b5f8973b79f701
TimH <tmherron09@gmail.com>
2020-08-03 12:17:56 -0500

    Add EndSweepstakes method of ISweepstakesManager interface

--- SweepstakesProject/ISweepstakeManager.cs
+++ SweepstakesProject/ISweepstakeManager.cs
@@ -8,9 +8,21 @@ namespace SweepstakesProject
 {
     public interface ISweepstakeManager
     {
+        /// <summary>
+        /// Inserts a Sweepstakes into the Sweepstakes Manager class implemeting ISweepstakesManager.
+        /// </summary>
+        /// <param name="sweepstakes">Sweepstakes to Insert into Collection of Sweepstakes.</param>
         void InsertSweepstakes(Sweepstakes sweepstakes);
 
+        /// <summary>
+        /// Method to return the current sweepstakes from a SweepstakesManager.
+        /// </summary>
+        /// <returns>Current Sweepstakes</returns>
         Sweepstakes GetSweepstakes();
+        /// <summary>
+        /// Method to end a sweepstakes. Removes Sweepstakes from SweepstakesManager Collection and calls methods needed to complete a sweepstakes.
+        /// </summary>
+        void EndSweepstakes();
 
     }
 }
