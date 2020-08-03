6d3146edc88b439b17b740b9ba92b60a9a12fc3b
TimH <tmherron09@gmail.com>
2020-08-03 11:34:58 -0500

    Implement SweepstakesStackManager definitions

--- SweepstakesProject/SweepstakesStackManager.cs
+++ SweepstakesProject/SweepstakesStackManager.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace SweepstakesProject
 {
-    class SweepstakesStackManager
+    public class SweepstakesStackManager : ISweepstakeManager
     {
+        Stack<Sweepstakes> stack;
+
+        /// <summary>
+        /// Constructs a new Sweepstakes Manager with an underlying <see cref="stack"/>
+        /// </summary>
+        public SweepstakesStackManager()
+        {
+            stack = new Stack<Sweepstakes>();
+        }
+
+        /// <summary>
+        /// Returns the current sweepstakes. Does not remove from the Stack.
+        /// </summary>
+        /// <returns>Current <see cref="Sweepstakes"/></returns>
+        public Sweepstakes GetSweepstakes()
+        {
+            return stack.Peek();
+        }
+        /// <summary>
+        /// Inserts a new SweepStakes to the top of the Stack. It will become the Current Sweepstakes until a new one is inserted or the Sweepstakes has been called to Complete.
+        /// </summary>
+        /// <param name="sweepstakes">Sweepstakes to add to campaign.</param>
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+            stack.Push(sweepstakes);
+        }
+        /// <summary>
+        /// Ends the current Sweepstakes found at the top of the stack and removes it.
+        /// </summary>
+        public void EndSweepstakes()
+        {
+            throw new NotImplementedException("Ending a campaign is not implemented.");
+            Sweepstakes sweepstakes = stack.Pop();
+        }
     }
 }
