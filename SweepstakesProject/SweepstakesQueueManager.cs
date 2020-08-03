46db0c8eb70770dc5bc0704ce7ac5c16a430a83d
TimH <tmherron09@gmail.com>
2020-08-03 12:17:18 -0500

    Implement SweepstakesQueueManager

--- SweepstakesProject/SweepstakesQueueManager.cs
+++ SweepstakesProject/SweepstakesQueueManager.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace SweepstakesProject
 {
-    class SweepstakesQueueManager
+    public class SweepstakesQueueManager : ISweepstakeManager
     {
+
+        Queue<Sweepstakes> queue;
+
+        /// <summary>
+        /// Constructs a new Sweepstakes Manager with an underlying data structure of 
+        /// </summary>
+        public SweepstakesQueueManager()
+        {
+            queue = new Queue<Sweepstakes>();
+        }
+
+        /// <summary>
+        /// Inserts a new Sweepstakes at the back of the Queue.
+        /// </summary>
+        /// <param name="sweepstakes">Sweepstakes to add to campaign.</param>
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+            queue.Enqueue(sweepstakes);
+        }
+        /// <summary>
+        /// Returns the current sweepstakes (at front of Queue.) Does not remove from Queue.
+        /// </summary>
+        /// <returns>Current <see cref="Sweepstakes"/></returns>
+        public Sweepstakes GetSweepstakes()
+        {
+            return queue.Peek();
+        }
+        /// <summary>
+        /// Ends the current sweepstakes and removes from Queue.
+        /// </summary>
+        public void EndSweepstakes()
+        {
+            throw new NotImplementedException();
+            Sweepstakes sweepstakes = queue.Dequeue();
+        }
+
     }
 }
